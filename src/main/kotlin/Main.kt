62075b7ee7cf5d12e9b17802475be18373473990
claddyk <madscientists1523@gmail.com>
2023-05-23 12:04:49 +0530

    Updated code to Handle errors

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,25 @@
 import kotlinx.cli.*
 import java.net.URL
+import java.net.HttpURLConnection
 
 fun fetchBlocks(startHeight: Int): String {
     val url = URL("https://mempool.space/api/blocks/$startHeight")
-    return url.readText()
+    return try {
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+        val responseCode = connection.responseCode
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            connection.inputStream.bufferedReader().use { it.readText() }
+        } else {
+            throw Exception("Failed to fetch blocks. Response code: $responseCode")
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        ""
+    }
 }
 
-fun main(args:Array<String>) {
+fun main(args: Array<String>) {
     val parser = ArgParser("block-explorer")
     val startHeight by parser.option(
         ArgType.Int,
