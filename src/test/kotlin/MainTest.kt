44062794cb3579b537b76371c6b0913026e9417f
Mohit Kumar <madscientists1523@gmail.com>
2023-06-01 18:47:21 +0530

    Updated unit tests, Status:All Passing

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,18 +1,40 @@
-import org.junit.jupiter.api.Assertions.assertEquals
+import kotlinx.cli.*
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.required
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import java.io.ByteArrayOutputStream
-import java.io.PrintStream
 
-class MainTest {
+class KotlinCliTest {
+
     @Test
-     fun testMain() {
-        val outContent = ByteArrayOutputStream()
-        System.setOut(PrintStream(outContent))
+    fun testFetchFirstBlockId() = runBlocking{
+        val parser = ArgParser("kotlin-cli")
+        val startHeight by parser.option(ArgType.Int, shortName = "s", description = "Start Height").required()
 
-        val args = arrayOf("-s", "1")
-        main(args)
+        parser.parse(arrayOf("-s", "730000"))
 
-        val expectedJson = """{"txids":["4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b","f9d9bcc4ef5f8c238c2cfcce7a8b6e5b4f9d49e833d9f6b8f4b9b41b1b9f4f5b"]}"""
-        assertEquals(expectedJson, outContent.toString().trim())
+        try {
+            val blockIds = fetchFirstBlockId(startHeight)
+            assertEquals("0000000000000000000384f28cb3b9cf4377a39cfd6c29ae9466951de38c0529", blockIds)
+        } catch (e: Exception) {
+            println("Error fetching block ID: $e")
+        }
+    }
+
+    @Test
+    fun testFetchTransactionId() = runBlocking{
+        val parser = ArgParser("kotlin-cli")
+        val startHeight by parser.option(ArgType.Int, shortName = "s", description = "Start Height").required()
+
+        parser.parse(arrayOf("-s", "1"))
+
+        try {
+            val firstTxId = fetchTransactionId("$startHeight")[0]
+            assertEquals("ce2ac5b6d807ae1368381a9ac5454d76ddb4b35742a87445aa145a78c5bc16d3", firstTxId)
+        } catch (e: Exception) {
+            println("Error fetching transaction ID: $e")
+        }
     }
 }
