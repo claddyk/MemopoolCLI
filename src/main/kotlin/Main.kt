097214a00f70d27e6f76f761716891ab104feb14
Mohit Kumar <madscientists1523@gmail.com>
2023-06-03 23:35:50 +0530

    Added support for multiple commands

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,47 @@
-
-import kotlinx.cli.ArgParser
-import kotlinx.cli.ArgType
-import kotlinx.cli.required
+import kotlinx.cli.*
 import kotlinx.coroutines.runBlocking
 
-fun main(args: Array<String>) = runBlocking() {
+@OptIn(ExperimentalCli::class)
+fun main(args: Array<String>) {
     val parser = ArgParser("kotlin-cli")
-    val startHeight by parser.option(ArgType.Int, shortName = "s", description = "Start Height").required()
-
-    parser.parse(args)
-
-    val apiClient = MempoolClient()
-
-    apiClient.fetchFirstBlockId(startHeight).fold(
-        onSuccess = {blockIds ->
-            apiClient.fetchTransactionIds(blockIds).fold(
-                onSuccess = { txIds -> txIds.forEach { println(it) } },
-                onFailure = { e-> println("error fetching transaction IDs: $e") }
-            )
-        },
-        onFailure = { e -> println("error fetching block ID: $e") }
+    parser.subcommands(
+        FetchTxIdsCommand(),
+        FetchBlockIdCommand()
     )
+    parser.parse(args)
+}
+
+@OptIn(ExperimentalCli::class)
+class FetchTxIdsCommand : Subcommand("fetchTxIds", "Fetch transaction IDs for a given block height") {
+    private val startHeight by option(ArgType.Int, shortName = "s", description = "Start Height").required()
+
+    override fun execute() = runBlocking{
+        val apiClient = MempoolClient()
+
+        apiClient.fetchFirstBlockId(startHeight).fold(
+            onSuccess = {blockIds ->
+                apiClient.fetchTransactionIds(blockIds).fold(
+                    onSuccess = { txIds -> txIds.forEach { println(it) } },
+                    onFailure = { e-> println("error fetching transaction IDs: $e") }
+                )
+            },
+            onFailure = { e -> println("error fetching block ID: $e") }
+        )
+    }
+}
+
+@OptIn(ExperimentalCli::class)
+class FetchBlockIdCommand : Subcommand("fetchBlockId", "Fetch block ID for a given block height") {
+    private val startHeight by option(ArgType.Int, shortName = "s", description = "Start Height").required()
+
+    override fun execute() = runBlocking{
+        val apiClient = MempoolClient()
+
+        apiClient.fetchFirstBlockId(startHeight).fold(
+            onSuccess = {blockId ->
+                println(blockId)
+            },
+            onFailure = { e -> println("error fetching block ID: $e") }
+        )
+    }
 }
