abb56e50681fd23aecfe85c790400ef085c5f979
Mohit Kumar <madscientists1523@gmail.com>
2023-06-06 17:18:57 +0530

    Updated MempoolTests

--- src/test/kotlin/MempoolTest.kt
+++ src/test/kotlin/MempoolTest.kt
@@ -1,6 +1,5 @@
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class MempoolClientTest {
@@ -18,5 +17,21 @@ class MempoolClientTest {
         val result = client.fetchTransactionIds("0000000000000000000384f28cb3b9cf4377a39cfd6c29ae9466951de38c0529")
         assertTrue(result.isSuccess)
         assertEquals("ce2ac5b6d807ae1368381a9ac5454d76ddb4b35742a87445aa145a78c5bc16d3", result.getOrThrow()[0])
+    }
+    @Test
+    fun testFetchFirstBlockId_InvalidStartHeight(): Unit = runBlocking {
+        val result = client.fetchFirstBlockId(-1) // Invalid start height
+        assertFalse(result.isSuccess)
+        assertTrue(result.isFailure)
+        assertEquals(emptyList<String>(), result.getOrDefault(emptyList<String>()))
+    }
+
+    @Test
+    fun testFetchTransactionId_InvalidBlockId(): Unit = runBlocking {
+        val invalidBlockId = "0000000000000000000384f28cabcdef4377a39cfabcdefe9466951de38c0529"
+        val result = client.fetchTransactionIds("0000000000000000000384f28cabcdef4377a39cfabcdefe9466951de38c0529")
+        assertFalse(result.isSuccess)
+        assertTrue(result.isFailure)
+        assertTrue(result.exceptionOrNull() is Exception)
     }
 }
