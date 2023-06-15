04028374f111fcdbf23a08d9b9f71135f30fb874
Mohit Kumar <madscientists1523@gmail.com>
2023-06-15 21:49:00 +0530

    Fixed Failing Tests

--- src/test/kotlin/FetchBlockIdFailing.kt
+++ src/test/kotlin/FetchBlockIdFailing.kt
@@ -1,19 +1,24 @@
-import kotlinx.cli.*
+
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ExperimentalCli
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import kotlin.test.assertNotNull
 
 @OptIn(ExperimentalCli::class)
 class FetchBlockIdCommandFailingTest {
 
     @Test
-    fun `test execute fetchBlockIdCommand with failing client`() {
+    fun `test execute fetchBlockIdCommand with failing client`() = runBlocking {
         val resultWriter = DummyResultWriter()
         val failingClient = FailingMempoolClient(RuntimeException("Mocked failure"))
+
         val command = FetchBlockIdCommand(resultWriter, failingClient)
         val parser = ArgParser("test")
         parser.subcommands(command)
         parser.parse(arrayOf("fetchBlockId", "-s", "730000"))
-
-        assertNotNull(resultWriter.lastError, "Mocked failure")
+        assertNull(resultWriter.lastResult)
+        assertNotNull(resultWriter.lastError)
+        assertEquals("Error fetching block ID: Mocked failure", resultWriter.lastError)
     }
 }
